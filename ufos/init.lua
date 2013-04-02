9a6156dfbc865b989bc45fd6fce7599c84f0df87
Zeg9 <dazeg9@gmail.com>
2013-04-02 19:04:05 +0200

    Use fuel to power ufos; add ufo charging device (ufo_furnace)

--- ufos/init.lua
+++ ufos/init.lua
@@ -1,18 +1,67 @@
 
-local UFO_SPEED = 10
-local UFO_TURN_SPEED = 2
+ufos = {}
 
-local ufo = {
+local UFO_SPEED = 1
+local UFO_TURN_SPEED = 2
+local UFO_MAX_SPEED = 10
+local UFO_FUEL_USE = .01
+
+ufos.fuel_from_wear = function(wear)
+	local fuel
+	if wear == 0 then
+		fuel = 0
+	else
+		fuel = (65535-(wear-1))*100/65535
+	end
+	return fuel
+end
+
+ufos.wear_from_fuel = function(fuel)
+	local wear = (100-(fuel))*65535/100+1
+	if wear > 65535 then wear = 0 end
+	return wear
+end
+
+ufos.get_fuel = function(self)
+	--return self.object:get_hp()
+	return self.fuel
+end
+
+ufos.set_fuel = function(self,fuel,object)
+	--[[if not object then object = self.object end
+	object:set_hp(fuel)]]
+	self.fuel = fuel
+end
+
+ufos.ufo_to_item = function(self)
+	local wear = ufos.wear_from_fuel(ufos.get_fuel(self))
+	return {name="ufos:ufo",wear=wear}
+end
+
+ufos.ufo_from_item = function(itemstack,placer,pointed_thing)
+	-- restore the fuel inside the item
+	local wear = itemstack:get_wear()
+	ufos.set_fuel(ufos.ufo,ufos.fuel_from_wear(wear))
+	-- add the entity
+	e = minetest.env:add_entity(pointed_thing.above, "ufos:ufo")
+	-- remove the item
+	itemstack:take_item()
+end
+
+
+ufos.ufo = {
 	physical = true,
 	collisionbox = {-1.5,-.5,-1.5, 1.5,2,1.5},
 	visual = "mesh",
 	mesh = "ufo.x",
-	textures = {"ufo.png"},
+	textures = {"ufo_0.png"},
 	
 	driver = nil,
-	v = 0
+	v = 0,
+	fuel = 0,
+	fueli = 0
 }
-function ufo:on_rightclick (clicker)
+function ufos.ufo:on_rightclick (clicker)
 	if not clicker or not clicker:is_player() then
 		return
 	end
@@ -25,65 +74,102 @@ function ufo:on_rightclick (clicker)
 	end
 end
 
-function ufo:on_activate (staticdata, dtime_s)
+function ufos.ufo:on_activate (staticdata, dtime_s)
 	self.object:set_armor_groups({immortal=1})
 end
 
-function ufo:on_punch (puncher, time_from_last_punch, tool_capabilities, direction)
+function ufos.ufo:on_punch (puncher, time_from_last_punch, tool_capabilities, direction)
 	self.object:remove()
 	if puncher and puncher:is_player() then
-		puncher:get_inventory():add_item("main", "ufos:ufo")
+		-- wear: save the fuel inside the itemstack
+		puncher:get_inventory():add_item("main", ufos.ufo_to_item(self))
 	end
 end
 
-function ufo:on_step (dtime)
+function ufos.ufo:on_step (dtime)
+	local fuel = ufos.get_fuel(self)
 	if self.driver then
 		local ctrl = self.driver:get_player_control()
 		local vel = self.object:getvelocity()
-		local acc = self.object:getacceleration()
-		if ctrl.up then
-			acc.x = math.cos(self.object:getyaw()+math.pi/2)*UFO_SPEED
-			acc.z = math.sin(self.object:getyaw()+math.pi/2)*UFO_SPEED
+		if fuel == nil then fuel = 0 end
+		if fuel > 0 and ctrl.up then
+			vel.x = vel.x + math.cos(self.object:getyaw()+math.pi/2)*UFO_SPEED
+			vel.z = vel.z + math.sin(self.object:getyaw()+math.pi/2)*UFO_SPEED
+			fuel = fuel - UFO_FUEL_USE
 		else
-			acc.x = -vel.x/5
-			acc.z = -vel.z/5
+			vel.x = vel.x*.99
+			vel.z = vel.z*.99
 		end
 		if ctrl.down then
-			acc.x = -vel.x
-			acc.z = -vel.z
+			vel.x = vel.x*.9
+			vel.z = vel.z*.9
 		end
-		if ctrl.jump then
-			vel.y = UFO_SPEED
-		elseif ctrl.sneak then
-			vel.y = -UFO_SPEED
+		if fuel > 0 and ctrl.jump then
+			vel.y = vel.y+UFO_SPEED
+			fuel = fuel - UFO_FUEL_USE
+		elseif fuel > 0 and ctrl.sneak then
+			vel.y = vel.y-UFO_SPEED
+			fuel = fuel - UFO_FUEL_USE
 		else
-			acc.y = -vel.y/2
+			vel.y = vel.y*.9
 		end
+		if vel.x > UFO_MAX_SPEED then vel.x = UFO_MAX_SPEED end
+		if vel.x < -UFO_MAX_SPEED then vel.x = -UFO_MAX_SPEED end
+		if vel.y > UFO_MAX_SPEED then vel.y = UFO_MAX_SPEED end
+		if vel.y < -UFO_MAX_SPEED then vel.y = -UFO_MAX_SPEED end
+		if vel.z > UFO_MAX_SPEED then vel.z = UFO_MAX_SPEED end
+		if vel.z < -UFO_MAX_SPEED then vel.z = -UFO_MAX_SPEED end
 		self.object:setvelocity(vel)
-		self.object:setacceleration(acc)
 		if ctrl.left then
 			self.object:setyaw(self.object:getyaw()+math.pi/120*UFO_TURN_SPEED)
 		end
 		if ctrl.right then
 			self.object:setyaw(self.object:getyaw()-math.pi/120*UFO_TURN_SPEED)
 		end
+		if ctrl.aux1 then
+			local pos = self.object:getpos()
+			local t = {{x=2,z=0},{x=-2,z=0},{x=0,z=2},{x=0,z=-2}}
+			for _, i in ipairs(t) do
+				pos.x = pos.x + i.x; pos.z = pos.z + i.z;
+				if minetest.env:get_node(pos).name == "ufos:furnace" then
+					meta = minetest.env:get_meta(pos)
+					if fuel < 100 and meta:get_int("charge") > 0 then
+						fuel = fuel + 1
+						meta:set_int("charge",meta:get_int("charge")-1)
+						meta:set_string("formspec", ufos.furnace_inactive_formspec
+							.. "label[0,0;Charge: "..meta:get_int("charge"))
+					end
+				end
+				pos.x = pos.x - i.x; pos.z = pos.z - i.z;
+			end
+		end
 	end
+	
+	if fuel < 0 then fuel = 0 end
+	if fuel > 100 then fuel = 100 end
+	if self.fueli ~= math.floor(fuel*8/100) then
+		self.fueli = math.floor(fuel*8/100)
+		print(self.fueli)
+		self.textures = {"ufo_"..self.fueli..".png"}
+		self.object:set_properties(self)
+	end
+	ufos.set_fuel(self,fuel)
 end
 
-minetest.register_entity("ufos:ufo", ufo)
+minetest.register_entity("ufos:ufo", ufos.ufo)
 
 
-minetest.register_craftitem("ufos:ufo", {
+minetest.register_tool("ufos:ufo", {
 	description = "ufo",
-	inventory_image = "ufo_inventory.png",
-	wield_image = "ufo_inventory.png",
+	inventory_image = "ufos_inventory.png",
+	wield_image = "ufos_inventory.png",
+	tool_capabilities = {load=0,max_drop_level=0, groupcaps={fleshy={times={}, uses=100, maxlevel=0}}},
 	
 	on_place = function(itemstack, placer, pointed_thing)
 		if pointed_thing.type ~= "node" then
 			return
 		end
-		minetest.env:add_entity(pointed_thing.above, "ufos:ufo")
-		itemstack:take_item()
+		ufos.ufo_from_item(itemstack,placer,pointed_thing)
 		return itemstack
 	end,
 })
@@ -92,8 +178,10 @@ minetest.register_craft( {
 	output = 'ufos:ufo',
 	recipe = {
 		{ "", "default:glass", ""},
-		{ "default:mese_crystal_fragment", "default:stone", "default:mese_crystal_fragment"},
-		{ "default:steel_ingot", "default:mese_crystal", "default:steel_ingot"},
+		{ "default:mese_crystal_fragment", "", "default:mese_crystal_fragment"},
+		{ "default:steelblock", "default:mese", "default:steelblock"},
 	},
 })
+
+dofile(minetest.get_modpath("ufos").."/furnace.lua")
 
